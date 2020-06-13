bdad613dffa9a4b9e12de92e4e932718c9b336c7
Pavel Shukhman <taleodor@gmail.com>
2020-06-13 16:13:16 -0400

    fix: retrieve images per namespace properly to ship to Reliza Hub on kubectl-reliza-client

--- kubectl-reliza-client/entrypoint.sh
+++ kubectl-reliza-client/entrypoint.sh
@@ -4,17 +4,13 @@ images_new=" "
 images_old=" "
 while [ true ]
 do
-    if [ "$NAMESPACE" == "allnamespaces" ]
-    then
-        images_new=$(kubectl get pods --all-namespaces -o jsonpath="{.items[*].status.containerStatuses[0].imageID}")
-    else
-        images_new=$(kubectl get pods -n $NAMESPACE -o jsonpath="{.items[*].status.containerStatuses[0].imageID}")
-    fi
+    images_new=$(kubectl get pods --all-namespaces -o jsonpath="{.items[*].status.containerStatuses[0].imageID}")
     if [ "$images_new" != "$images_old" ]
     then
         images_old="$images_new"
         NAMESPACES=""
         echo "$(date) - change in images detected - shipping images to Reliza Hub"
+        echo "new images = $images_new"
         if [ "$NAMESPACE" == "allnamespaces" ]
         then
             readarray -t NAMESPACES < <(kubectl get ns -o custom-columns=NAME:.metadata.name)
@@ -24,7 +20,9 @@ do
         for ns in "${NAMESPACES[@]}"; do
             if [ $ns != "NAME" ]
             then
-                /app/app instdata -u $HUB_URI -i $RELIZA_API_ID -k $RELIZA_API_KEY --sender $SENDER_ID --namespace $ns --images "$images_new"
+                images=$(kubectl get pods -n $ns -o jsonpath="{.items[*].status.containerStatuses[0].imageID}")
+                echo "$(date) shipping images for ns $ns = $images"
+                /app/app instdata -u $HUB_URI -i $RELIZA_API_ID -k $RELIZA_API_KEY --sender $SENDER_ID --namespace $ns --images "$images"
             fi
         done
     fi
