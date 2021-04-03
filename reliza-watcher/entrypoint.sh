2bd291c570be801f6cca714c060a395b93fcc0e4
Pavel Shukhman <taleodor@gmail.com>
2021-04-03 17:41:52 -0400

    fix: correct entrypoint for shipping k8s images in reliza-watcher

--- reliza-watcher/entrypoint.sh
+++ reliza-watcher/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-images_new=" "
 # cache to limit api usage
-images_old=" "
 while [ true ]
 do
     cp /resources/images /resources/images_old
-    kubectl get po -o json | jq "[{namespace:.items[].metadata.namespace, pod:.items[].metadata.name, status:.items[].status.containerStatuses[]}] > /resources/images
+    kubectl get po --all-namespaces -o json | jq "[{namespace:.items[].metadata.namespace, pod:.items[].metadata.name, status:.items[].status.containerStatuses[]}]" > /resources/images
     difflines=$(diff /resources/images /resources/images_old | wc -l)
     if [ $difflines -gt 0 ]
     then
         echo "$(date) - change in images detected - shipping images to Reliza Hub"
-        echo "new images = $images_new"
         if [ "$NAMESPACE" == "allnamespaces" ]
         then
             readarray -t NAMESPACES < <(kubectl get ns -o custom-columns=NAME:.metadata.name)
@@ -20,8 +17,9 @@ do
         for ns in "${NAMESPACES[@]}"; do
             if [ $ns != "NAME" ]
             then
-                echo "$(date) shipping images for ns $ns"
-                /app/app instdata -u $HUB_URI -i $RELIZA_API_ID -k $RELIZA_API_KEY --sender $SENDER_ID --namespace $ns --imagestyle k8s --imagefile /resources/images
+                kubectl get po -n $ns -o json | jq "[{namespace:.items[].metadata.namespace, pod:.items[].metadata.name, status:.items[].status.containerStatuses[]}]" > /resources/images_to_send
+                echo "$(date) shipping images for $ns namespace"
+                /app/app instdata -u $HUB_URI -i $RELIZA_API_ID -k $RELIZA_API_KEY --sender $SENDER_ID --namespace $ns --imagestyle k8s --imagefile /resources/images_to_send
             fi
         done
     fi
