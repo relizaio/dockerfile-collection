f093059547445007688130db9d9a2c83d3c90ad7
Pavel Shukhman <taleodor@gmail.com>
2021-04-10 19:15:12 -0400

    fix: combine sender id and namespace as sender id to allow sending from multiple namespaces

--- reliza-watcher/entrypoint.sh
+++ reliza-watcher/entrypoint.sh
@@ -19,7 +19,7 @@ do
             then
                 kubectl get po -n $ns -o json | jq "[.items[] | {namespace:.metadata.namespace, pod:.metadata.name, status:.status.containerStatuses[]}]" > /resources/images_to_send
                 echo "$(date) shipping images for $ns namespace"
-                /app/app instdata -u $HUB_URI -i $RELIZA_API_ID -k $RELIZA_API_KEY --sender $SENDER_ID --namespace $ns --imagestyle k8s --imagefile /resources/images_to_send
+                /app/app instdata -u $HUB_URI -i $RELIZA_API_ID -k $RELIZA_API_KEY --sender $SENDER_ID$ns --namespace $ns --imagestyle k8s --imagefile /resources/images_to_send
             fi
         done
     fi
