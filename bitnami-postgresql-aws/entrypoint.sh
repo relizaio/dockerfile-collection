ca5cb26e8423498d6d11853bb6374603a0dc4fbf
Pavel Shukhman <taleodor@gmail.com>
2021-12-05 11:34:26 -0500

    fix: correct path to gzip in psql awscli entrypoint

--- bitnami-postgresql-aws/entrypoint.sh
+++ bitnami-postgresql-aws/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/bitnami/postgresql/bin/pg_dump -U $PG_USER -h $PG_HOST $PG_DATABASE | /usr/bin/gzip > /tmp/dump.gz && /usr/local/bin/aws s3 cp /tmp/dump.gz s3://$AWS_BUCKET/$DUMP_PREFIX-$(date +"%Y-%m-%d-%H-%M").gz
+/opt/bitnami/postgresql/bin/pg_dump -U $PG_USER -h $PG_HOST $PG_DATABASE | /bin/gzip > /tmp/dump.gz && /usr/local/bin/aws s3 cp /tmp/dump.gz s3://$AWS_BUCKET/$DUMP_PREFIX-$(date +"%Y-%m-%d-%H-%M").gz
