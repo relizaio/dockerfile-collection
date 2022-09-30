69a78ae3a0c72a2747dbf0347a511915448716d8
Pavel Shukhman <taleodor@gmail.com>
2022-09-30 12:05:40 -0400

    fix: psql awscli entrypoint from bash to sh

--- bitnami-postgresql-aws/entrypoint.sh
+++ bitnami-postgresql-aws/entrypoint.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 /usr/bin/pg_dump -U $PG_USER -h $PG_HOST $PG_DATABASE | /bin/gzip > /tmp/dump.gz && /usr/local/bin/aws s3 cp /tmp/dump.gz s3://$AWS_BUCKET/$DUMP_PREFIX-$(date +"%Y-%m-%d-%H-%M").gz
