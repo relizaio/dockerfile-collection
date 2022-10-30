4aeee82d19d2c74d2f88705bbbd1a0316f2a2dc1
Pavel Shukhman <taleodor@gmail.com>
2022-10-30 16:48:55 -0400

    fix: fail backups on erros

--- bitnami-postgresql-aws/entrypoint.sh
+++ bitnami-postgresql-aws/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+set -o errexit
+set -o nounset
+set -o pipefail
 /usr/bin/pg_dump -U $PG_USER -h $PG_HOST $PG_DATABASE | /bin/gzip > /tmp/dump.gz && /usr/local/bin/aws s3 cp /tmp/dump.gz s3://$AWS_BUCKET/$DUMP_PREFIX-$(date +"%Y-%m-%d-%H-%M").gz
