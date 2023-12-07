929459e1e60d1641f3a3f6656420385d5205285c
Rhythm Garg <rhythmgarg18@gmail.com>
2023-12-07 15:20:12 +0530

    fix: refactor reliza-watcher

--- reliza-watcher/entrypoint.sh
+++ reliza-watcher/entrypoint.sh
@@ -7,7 +7,7 @@ send_data () {
     echo "$(date) - change in images detected - shipping images to Reliza Hub"
     if [ "$NAMESPACE" == "allnamespaces" ]
     then
-        readarray -t NAMESPACES < <(kubectl get ns -o custom-columns=NAME:.metadata.name)
+        readarray -t NAMESPACES < <(./kubectl get ns -o custom-columns=NAME:.metadata.name)
     else
         IFS="," read -ra NAMESPACES <<< "$NAMESPACE"
     fi
@@ -19,39 +19,39 @@ send_data () {
     for ns in "${NAMESPACES[@]}"; do
         if [ $ns != "NAME" ]
         then
-            kubectl get po -n $ns -o json | jq "[.items[] | {namespace:.metadata.namespace, labels:.metadata.labels, annotations:.metadata.annotations, pod:.metadata.name, status:.status.containerStatuses[]}]" > /resources/images_to_send
+            ./kubectl get po -n $ns -o json | jq "[.items[] | {namespace:.metadata.namespace, labels:.metadata.labels, annotations:.metadata.annotations, pod:.metadata.name, status:.status.containerStatuses[]}]" > ./resources/images_to_send
             echo "$(date) shipping images for $ns namespace"
             
-            /app/app instdata -u $relizaHubUri -i $RELIZA_API_ID -k $RELIZA_API_KEY --sender $SENDER_ID$ns --namespace $ns --imagestyle k8s --imagefile /resources/images_to_send
+            ./reliza-cli instdata -u $relizaHubUri -i $RELIZA_API_ID -k $RELIZA_API_KEY --sender $SENDER_ID$ns --namespace $ns --imagestyle k8s --imagefile ./resources/images_to_send
             
             # record last sent timestamp
             if [ $record_timestamp -eq 1 ]
             then
-                date +"%s" > /resources/last_sent
+                date +"%s" > ./resources/last_sent
             fi
         fi
     done
 }
 
-date +"%s" > /resources/last_sent
+date +"%s" > ./resources/last_sent
 while [ true ]
 do
     record_timestamp=0
-    cp /resources/images /resources/images_old
+    cp ./resources/images ./resources/images_old
     if [ "$NAMESPACE" == "allnamespaces" ]
     then
-      kubectl get po --all-namespaces -o json | jq "[.items[] | {namespace:.metadata.namespace, labels:.metadata.labels, annotations:.metadata.annotations, pod:.metadata.name, status:.status.containerStatuses[]}]" > /resources/images
+      ./kubectl get po --all-namespaces -o json | jq "[.items[] | {namespace:.metadata.namespace, labels:.metadata.labels, annotations:.metadata.annotations, pod:.metadata.name, status:.status.containerStatuses[]}]" > ./resources/images
     else
-      echo "images start" > /resources/images
+      echo "images start" > ./resources/images
       IFS="," read -ra NAMESPACESCHECK <<< "$NAMESPACE"
       for nsc in "${NAMESPACESCHECK[@]}"; do
         if [ $nsc != "NAME" ]
         then
-            kubectl get po -n $nsc -o json | jq "[.items[] | {namespace:.metadata.namespace, labels:.metadata.labels, annotations:.metadata.annotations, pod:.metadata.name, status:.status.containerStatuses[]}]" >> /resources/images
+            ./kubectl get po -n $nsc -o json | jq "[.items[] | {namespace:.metadata.namespace, labels:.metadata.labels, annotations:.metadata.annotations, pod:.metadata.name, status:.status.containerStatuses[]}]" >> ./resources/images
         fi
       done
     fi
-    difflines=$(diff /resources/images /resources/images_old | wc -l)
+    difflines=$(diff ./resources/images ./resources/images_old | wc -l)
     if [ $difflines -gt 0 ]
     then
         record_timestamp=1
@@ -59,7 +59,7 @@ do
     # else
         # send follow ups to ensure we converge properly
     #    record_timestamp=0
-    #    if [ $(expr $(date +"%s") - $(cat /resources/last_sent)) -lt 30 ]
+    #    if [ $(expr $(date +"%s") - $(cat ./resources/last_sent)) -lt 30 ]
     #    then
     #        send_data
     #    fi
